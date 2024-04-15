73e51c2d43307349930f351c7ce104cba07b3ee8
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-04-15 12:25:35 +0300

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paternu__4
+{
+    public class Ball
+    {
+        protected double[] coefficients;
+        private double radius;
+
+
+        public Ball(double[] coefficients, double radius)
+        {
+            this.coefficients = coefficients;
+            this.radius = radius;
+        }
+
+        public virtual void DisplayShape()
+        {
+            Console.WriteLine("Форма: Куля");
+        }
+
+        // Метод для задання нових коефіцієнтів кулі
+        public void SetCoefficients(double[] coefficients)
+        {
+            this.coefficients = coefficients;
+        }
+
+        // Метод для виведення коефіцієнтів на екран консолі
+        public void DisplayCoefficients()
+        {
+            Console.WriteLine("Coefficients:");
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                Console.WriteLine($"b{i + 1} = {coefficients[i]}");
+            }
+        }
+
+        // Метод для обчислення об'єму кулі
+        public virtual double CalculateVolume()
+        {
+            // формула V = (4/3) * π * r^3
+            double volume = (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+            return volume;
+        }
+    }
+}
---
+++ Ellipsoid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paternu__4
+{
+    public class Ellipsoid : Ball
+    {
+        public Ellipsoid(double[] coefficients, double radius) : base(coefficients, radius)
+        {
+            // Конструктор класу Ellipsoid викликає конструктор базового класу Ball
+        }
+
+        public override void DisplayShape()
+        {
+            Console.WriteLine("Форма: Еліпсоїд");
+        }
+
+        // Перевантажуємо метод задання відповідних коефіцієнтів
+        public new void SetCoefficients(double[] coefficients)
+        {
+            // Ми використовуємо ключове слово "new", щоб вказати, що перевизначаємо метод базового класу
+            base.SetCoefficients(coefficients); // Викликаємо метод базового класу для встановлення коефіцієнтів
+        }
+
+        // Перевантажуємо метод виведення відповідних коефіцієнтів на екран
+        public new void DisplayCoefficients()
+        {
+           
+            base.DisplayCoefficients(); 
+        }
+
+        // Метод для обчислення об'єму еліпсоїда
+        public override double CalculateVolume()
+        {
+            double volume = (4.0 / 3.0) * Math.PI * coefficients[0] * coefficients[1] * coefficients[2];
+            return volume;
+        }
+    }
+}
